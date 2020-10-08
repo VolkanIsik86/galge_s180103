e490d8150657f0dea735afe956fcf8ac76772011
volkanisik <volkanisik@live.dk>
2020-10-08 17:22:03 +0200

    nyt grafik

--- app/src/main/java/com/example/volkangalge/MainActivity.java
+++ app/src/main/java/com/example/volkangalge/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.volkangalge;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button start;
+    Button afslut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        start = findViewById(R.id.start);
+        afslut = findViewById(R.id.afslut);
+
+        start.setOnClickListener(this);
+        afslut.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        if(view==start){
+            Intent start = new Intent(this,GameActivity.class);
+            startActivity(start);
+
+        } else if (view == afslut){
+            finish();
+        }
+
     }
 }
